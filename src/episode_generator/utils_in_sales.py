ea85782a4516fd1c2671890e1fb4ef753443dbc0
wkwang <wkwangnlp@gmail.com>
2018-05-26 13:23:10 +0800

    init

--- src/episode_generator/utils_in_sales.py
+++ src/episode_generator/utils_in_sales.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import random, os, json
-from utils import random_pick
+from utils import random_pick, filter_p_dict
 from config import *
 
 
@@ -30,30 +30,33 @@ def in_sale_generator(script, intent, grammar_p_dict, given_entity, entity):
 
 
 def in_sales_controller(script, available_intent, grammar_p_dict, intent_p_dict, desired_entity):
+    available_intent_p_dict = filter_p_dict(available_intent, intent_p_dict)
     pick_num = random.randint(1, len(available_intent))
-    picked_intent = random_pick(list(intent_p_dict.keys()), list(intent_p_dict.values()), pick_num)
-    if type(picked_intent) is not list:
-        picked_intent = [picked_intent]
+    picked_intents = random_pick(list(available_intent_p_dict.keys()), list(available_intent_p_dict.values()), pick_num)
+    if type(picked_intents) is not list:
+        picked_intents = [picked_intents]
 
     # make topic coherent
     scene_dict = {'pay': [], 'express': []}
-    for intent in picked_intent:
+    for intent in picked_intents:
         if intent in ['discountURL', 'payment']:
             scene_dict['pay'].append(intent)
         else:
             scene_dict['express'].append(intent)
+    scene_keys = list(scene_dict.keys())
+    random.shuffle(scene_keys)
 
-    picked_intent = list()
-    for key in scene_dict.keys():
+    picked_intents = list()
+    for key in scene_keys:
         scene_content = scene_dict[key]
         random.shuffle(scene_content)
-        picked_intent.extend(scene_content)
+        picked_intents.extend(scene_content)
 
     # generate scene
     given_entity = False
     in_sales_script = list()
 
-    for intent in picked_intent:
+    for intent in picked_intents:
         scene, given_entity = in_sale_generator(script[intent], intent, grammar_p_dict[intent], given_entity,
                                                 desired_entity)
         in_sales_script.append(scene)
