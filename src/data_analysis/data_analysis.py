806802bd563272ed9628d57c5bb05a5ab931dea5
wkwang <wkwangnlp@gmail.com>
2018-06-19 10:42:57 +0800

    Fix bug in sentiment when nfc and network is not good but sentiment is positive.

--- src/data_analysis/data_analysis.py
+++ src/data_analysis/data_analysis.py
@@ -10,7 +10,7 @@ class DataAnalysis(object):
     def analysis():
         for task in TASKS.keys():
             for data_set_name in DATA_SET.keys():
-                with open(os.path.join(DATA_ROOT, "public_1", data_set_name, task + ".json"), "rb") as f:
+                with open(os.path.join(DATA_ROOT, "public_1", task, data_set_name + ".json"), "rb") as f:
                     data = json.load(f)
 
                 # todo: we can add more statistical factor!
@@ -24,7 +24,7 @@ class DataAnalysis(object):
                 results["agent_answer"] = sorted(agent_answer)
 
                 print(str(len(results["agent_answer"])) + " system actions we found in " + task + " " + data_set_name)
-                with io.open(os.path.join(DATA_ROOT, "statistics", data_set_name, task + ".json"), "w",
+                with io.open(os.path.join(DATA_ROOT, "statistics", task, data_set_name + ".json"), "w",
                              encoding="utf-8") as f:
                     json.dump(results, f, ensure_ascii=False, indent=4)
 
--- src/parse_data/parse_data.py
+++ src/parse_data/parse_data.py
@@ -3,17 +3,39 @@
 """
 This file parse data like babi-task
 """
-
+from config import *
 import json
 import os
 import io
 import jieba
-from config import *
+import re
 
 
 class ParseData(object):
+    def __init__(self, user_dict="specialSign.txt"):
+        with open(os.path.join(DATA_ROOT, user_dict), "rb") as f:
+            self.specialSign = f.readlines()
+
     @staticmethod
-    def parse_candidate():
+    def cut(l):
+        special_sign = re.findall(r"\$\S+?\$", l)
+        l = re.sub(r"\$\S+?\$", "PLACEHOLDER", l)
+        cut_l = list()
+        for word in jieba.cut(l):
+            if word == "PLACEHOLDER":
+                cut_l.append(special_sign[0])
+                del special_sign[0]
+            else:
+                cut_l.append(word)
+        return " ".join(cut_l)
+
+    def get_vocab(self):
+        for task in TASKS.keys():
+            vocab = dict()
+            for data_set_name in DATA_SET.keys():
+                with open(os.path.join(DATA_ROOT, "public_1"))
+
+    def parse_candidate(self):
         for task in TASKS.keys():
             candidate = list()
             for data_set_name in DATA_SET.keys():
@@ -21,9 +43,14 @@ class ParseData(object):
                     data = json.load(f)
                 candidate.extend(data["agent_answer"])
 
-            with io.open(os.path.join(DATA_ROOT, "candidate", task + ".json")) as f:
-                for line in candidate:
-                    f.write(line + "\n")
+            with io.open(os.path.join(DATA_ROOT, "candidate", task + ".txt"), "w", encoding="utf-8") as f:
+                for line in set(candidate):
+                    f.write(self.cut(line) + "\n")
 
-    @staticmethod
-    def parse_dialog():
+    def parse_dialog(self):
+        pass
+
+
+if __name__ == "__main__":
+    data_parser = ParseData()
+    data_parser.parse_candidate()
