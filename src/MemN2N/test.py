a847275563d0bdc9d34e651175c1adb3bb7b0949
wkwang <wkwangnlp@gmail.com>
2018-06-26 19:30:58 +0800

    init Memory Network

--- src/MemN2N/test.py
+++ src/MemN2N/test.py
@@ -15,20 +15,14 @@ def _parse_args():
 
     parser.add_argument("--trained_task", type=str)
     parser.add_argument("--trained_model", default=None, type=str)
-    parser.add_argument("--test_task", type=str)
+    parser.add_argument("--testing_task", type=str)
     parser.add_argument("--emb_dim", default=32, type=int)
-    parser.add_argument("--save_dir", default="checkpoints", type=str)
-    parser.add_argument("--learning_rate", type=float, default=0.001)
     parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--epochs", type=int, default=50)
     parser.add_argument("--random_seed", type=int, default=42)
     parser.add_argument("--cuda", action="store_true")
     parser.add_argument("--memory_size", type=int, default=50)
     parser.add_argument("--max_hops", type=int, default=3)
     parser.add_argument("--nonlinear", type=str, default="iden")
-    parser.add_argument("--max_clip", type=float, default=40)
-    parser.add_argument("--noise_stddev", type=float, default=0.1)
-    parser.add_argument("--evaluation_interval", type=int, default=2)
 
     return parser.parse_args()
 
@@ -37,31 +31,25 @@ if __name__ == "__main__":
     args = _parse_args()
     args.device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
-    # checkpoints path
-    args.save_dir = "/".join([args.save_dir, args.task])
-
-    train_file = os.path.join(DATA_ROOT, "public", args.task, "train.txt")
-    dev_file = os.path.join(DATA_ROOT, "public", args.task, "dev.txt")
-    candidates_file = os.path.join(DATA_ROOT, "candidate", args.task + ".txt")
+    test_file = os.path.join(DATA_ROOT, "public", args.testing_task, "test.txt")
+    candidates_file = os.path.join(DATA_ROOT, "candidate", args.trained_task + ".txt")
 
     data_utils = DataUtils()
-    data_utils.load_vocab(args.task)
-    data_utils.load_candidates(args.task)
-    train_data = data_utils.load_dialog(train_file)
-    dev_data = data_utils.load_dialog(dev_file)
-    data = train_data + dev_data
-    data_utils.build_pad_config(data, args.memory_size)
+    data_utils.load_vocab(args.trained_task)
+    data_utils.load_candidates(args.trained_task)
+    test_data = data_utils.load_dialog(test_file)
+    data_utils.build_pad_config(test_data, args.memory_size)
     candidates_vec = data_utils.vectorize_candidates()
 
     model = MemN2N(data_utils.vocab_size, args.emb_dim, args.max_hops, args.nonlinear, candidates_vec,
                    args.random_seed).to(args.device)
 
-    if args.trained_model is not None:
-        with open(args.trained_model, "rb") as f:
-            model.load_state_dict(torch.load(f))
+    assert args.trained_model is not None
+    with open(args.trained_model, "rb") as f:
+        model.load_state_dict(torch.load(f))
 
     config = {"lr": args.learning_rate, "epochs": args.epochs, "device": args.device, "batch_size": args.batch_size,
               "save_dir": args.save_dir, "max_clip": args.max_clip, "noise_stddev": args.noise_stddev,
               "random_seed": args.random_seed, "evaluation_interval": args.evaluation_interval}
-    agent = MemAgent(config, model, train_data, dev_data, None, data_utils)
-    agent.train()
+    agent = MemAgent(config, model, None, None, test_data, data_utils)
+    agent.test()
