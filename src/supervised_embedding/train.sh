fb3daa9219cccd63a53b6451ab266199068bceed
wkwang <wkwangnlp@gmail.com>
2018-07-02 11:22:11 +0800

    init Memory Network

--- src/supervised_embedding/train.sh
+++ src/supervised_embedding/train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_1 --cuda > task1_debug.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_2 --cuda > task2_debug.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_3 --cuda > task3_debug.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_4 --cuda > task4_debug.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_5 --cuda > task5_debug.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_1 --cuda > debug/train_task1.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_2 --cuda > debug/train_task2.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_3 --cuda > debug/train_task3.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_4 --cuda > debug/train_task4.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python train.py --task task_5 --cuda > debug/train_task5.log 2>&1 &
