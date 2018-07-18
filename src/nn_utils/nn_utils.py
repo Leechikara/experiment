e3cfb0016ff76dbebc62373a3ba36e655fbb9f1b
wkwang <wkwangnlp@gmail.com>
2018-07-18 09:38:06 +0800

    init VAE

--- src/nn_utils/nn_utils.py
+++ src/nn_utils/nn_utils.py
@@ -7,41 +7,47 @@ import torch.nn.functional as F
 
 
 # sentence embedding method
-def get_bow(embedding, avg=True):
+def get_bow(embedding, emb_sum=False):
     """
     Assumption, the last dimension is the embedding
     The second last dimension is the sentence length.
     The rank must be not less than 3.
     """
     embedding_size = embedding.size(-1)
-    if avg:
-        return embedding.mean(-2), embedding_size
-    else:
+    if emb_sum:
         return embedding.sum(-2), embedding_size
+    else:
+        return embedding.mean(-2), embedding_size
 
 
 class Attn(nn.Module):
-    def __init__(self, method, hidden_size):
+    def __init__(self, method, encode_hidden_size, decode_hidden_size):
         super(Attn, self).__init__()
-        assert method in ["dotted", "general", "concat"]
-        self.method = method
-        self.hidden_size = hidden_size
+        if method.lower() not in ["dotted", "general", "concat"]:
+            raise RuntimeError("Attention methods should be dotted, general or concat but get {}!".format(method))
+        if method.lower() == "dotted" and encode_hidden_size != decode_hidden_size:
+            raise RuntimeError("In dotted attention, the encode_hidden_size should equal to decode_hidden_size.")
+
+        self.method = method.lower()
+        self.encode_hidden_size = encode_hidden_size
+        self.decode_hidden_size = decode_hidden_size
 
         if self.method == "general":
-            self.attn = nn.Linear(hidden_size, hidden_size)
+            self.attn = nn.Linear(self.encode_hidden_size, self.decode_hidden_size)
         elif self.method == "concat":
             self.attn = nn.Sequential(
-                nn.Linear(hidden_size * 2, hidden_size),
+                nn.Linear((self.encode_hidden_size + self.decode_hidden_size),
+                          (self.encode_hidden_size + self.decode_hidden_size) / 2),
                 nn.Tanh(),
-                nn.Linear(hidden_size, 1)
+                nn.Linear((self.encode_hidden_size + self.decode_hidden_size) / 2, 1)
             )
 
     def forward(self, encode_outputs, decode_state):
         # todo: add mask
         """
-        :param encode_outputs: (batch, output_length, hidden_size)
-        :param decode_state: (batch, hidden_size)
-        :return: (batch, output_length)
+        :param encode_outputs: (batch, output_length, encode_hidden_size)
+        :param decode_state: (batch, decode_hidden_size)
+        :return: probs: (batch, output_length), attention results: (batch, encode_hidden_size)
         """
         output_length = encode_outputs.size(1)
         if self.method == "concat":
@@ -57,4 +63,6 @@ class Attn(nn.Module):
             decode_state_temp = decode_state.unsqueeze(1)
             energy = torch.sum(decode_state_temp * encode_outputs, 2)
         probs = F.softmax(energy, dim=1)
-        return probs
+        probs_temp = probs.unsqueeze(2)
+        results = torch.sum(probs_temp * encode_outputs, 1)
+        return probs, results
