1eacd35c1f7a9518e9431def5a705ddd9284a57d
wkwang <wkwangnlp@gmail.com>
2018-07-24 11:05:43 +0800

    init VAE

--- src/Continuous_VAE/run_config.py
+++ src/Continuous_VAE/run_config.py
@@ -12,24 +12,52 @@ class RunConfig(object):
     max_clip = 40.0
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # context encoding config
-    context_encode_method = "MemoryNetwork"
-    memory_size = 50
-    max_hops = 3
-    memory_nonlinear = "iden"
+    # word embedding config
+    word_emb_dim = 32
 
     # sentence encoding config
     sent_encode_method = "bow"
     emb_sum = False
-    rnn_type = "gru"
-    rnn_hidden_size = 32
-    rnn_layers = 1
-    rnn_dropout = 0
-    bidirectional = True
+    sent_rnn_type = "gru"
+    sent_rnn_hidden_size = 32
+    sent_rnn_layers = 1
+    sent_rnn_dropout = 0
+    sent_rnn_bidirectional = True
+
+    # context encoding config
+    ctx_encode_method = "MemoryNetwork"
+    attn_method = "general"
+    memory_size = 50
+    max_hops = 3
+    memory_nonlinear = "iden"
+    ctx_rnn_type = "gru"
+    ctx_rnn_hidden_size = 32
+    ctx_rnn_layers = 1
+    ctx_rnn_dropout = 0
+    ctx_rnn_bidirectional = True
 
     self_att = False
-    attn_method = "general"
+    self_att_hidden = 32
+    self_att_head = 1
 
     latent_size = 20
     sample = 50
     threshold = 0.7
+
+    if sent_encode_method == "rnn":
+        if self_att is True:
+            sent_emb_dim = sent_rnn_hidden_size * 2 if sent_rnn_bidirectional else sent_rnn_hidden_size
+        else:
+            sent_emb_dim = sent_rnn_hidden_size * sent_rnn_layers * 2 if sent_rnn_bidirectional \
+                else sent_rnn_hidden_size * sent_rnn_layers
+    elif sent_encode_method == "bow":
+        sent_emb_dim = word_emb_dim
+
+    if ctx_encode_method == "MemoryNetwork":
+        ctx_emb_dim = sent_emb_dim
+    elif ctx_encode_method == "HierarchalRNN" or ctx_encode_method == "RNN":
+        if self_att is True:
+            ctx_emb_dim = ctx_rnn_hidden_size * 2 if ctx_rnn_bidirectional else ctx_rnn_hidden_size
+        else:
+            ctx_emb_dim = ctx_rnn_hidden_size * ctx_rnn_layers * 2 if ctx_rnn_bidirectional \
+                else ctx_rnn_hidden_size * ctx_rnn_layers
