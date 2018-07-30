19cc783bdb3d90494525b5c35afd5a009bd30017
wkwang <wkwangnlp@gmail.com>
2018-07-30 19:51:05 +0800

    init VAE

--- src/Continuous_VAE/model/utils.py
+++ src/Continuous_VAE/model/utils.py
@@ -4,9 +4,9 @@ import torch
 
 
 def gaussian_kld(recog_mu, recog_logvar, prior_mu, prior_logvar):
-    kld = -0.5 * torch.sum(1 + (recog_logvar - prior_logvar)
-                           - torch.div(torch.pow(prior_mu - recog_mu, 2), torch.exp(prior_logvar))
-                           - torch.div(torch.exp(recog_logvar), torch.exp(prior_logvar)), 1)
+    kld = 0.5 * torch.sum(1 + (recog_logvar - prior_logvar)
+                          - torch.div(torch.pow(prior_mu - recog_mu, 2), torch.exp(prior_logvar))
+                          - torch.div(torch.exp(recog_logvar), torch.exp(prior_logvar)), 1)
     return kld
 
 
