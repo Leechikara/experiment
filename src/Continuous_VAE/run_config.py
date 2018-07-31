5f78d904dad94f45924f5fa082a52baa1d9631bf
wkwang <wkwangnlp@gmail.com>
2018-07-31 15:09:21 +0800

    init VAE

--- src/Continuous_VAE/run_config.py
+++ src/Continuous_VAE/run_config.py
@@ -34,18 +34,23 @@ class RunConfig(object):
     ctx_rnn_hidden_size = 32
     ctx_rnn_layers = 1
     ctx_rnn_dropout = 0
-    ctx_rnn_bidirectional = True
+    ctx_rnn_bidirectional = False
 
-    self_attn = True
-    self_attn_hidden = 32
-    self_attn_head = 1
+    sent_self_attn = True
+    sent_self_attn_hidden = 32
+    sent_self_attn_head = 1
+
+    ctx_self_attn = False
+    ctx_self_attn_hidden = 32
+    ctx_self_attn_head = 1
 
     latent_size = 20
     sample = 50
     threshold = 0.7
+    full_kl_step = 10000
 
     if sent_encode_method == "rnn":
-        if self_attn is True:
+        if sent_self_attn is True:
             sent_emb_size = sent_rnn_hidden_size * 2 if sent_rnn_bidirectional else sent_rnn_hidden_size
         else:
             sent_emb_size = sent_rnn_hidden_size * sent_rnn_layers * 2 if sent_rnn_bidirectional \
@@ -53,10 +58,10 @@ class RunConfig(object):
     elif sent_encode_method == "bow":
         sent_emb_size = word_emb_size
 
-    if ctx_encode_method == "MemoryNetwork":
+    if ctx_encode_method == "MemoryNetwork" or ctx_encode_method == "HierarchalSelfAttn":
         ctx_emb_size = sent_emb_size
-    elif ctx_encode_method == "HierarchalRNN" or ctx_encode_method == "RNN":
-        if self_attn is True:
+    elif ctx_encode_method == "HierarchalRNN":
+        if ctx_self_attn is True:
             ctx_emb_size = ctx_rnn_hidden_size * 2 if ctx_rnn_bidirectional else ctx_rnn_hidden_size
         else:
             ctx_emb_size = ctx_rnn_hidden_size * ctx_rnn_layers * 2 if ctx_rnn_bidirectional \
