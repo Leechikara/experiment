537e282c5247bb451e43ec6260e6f011a32443e0
wkwang <wkwangnlp@gmail.com>
2018-08-01 10:45:39 +0800

    init VAE

--- src/Continuous_VAE/model/cvae.py
+++ src/Continuous_VAE/model/cvae.py
@@ -490,7 +490,7 @@ class ContinuousAgent(object):
             loss_log["avg_kld_loss"].append(avg_kld_item)
             loss_log["acc"].append(acc)
 
-        torch.save(self.model.state_dict(), self.config.save_path)
+        torch.save(self.model.state_dict(), self.config.model_save_path)
         pickle.dump(loss_log, open(self.config.debug_path, "wb"))
 
         # for debug
